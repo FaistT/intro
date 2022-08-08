36b869e592d07e19b2b1134531abe67544af19d7
FaistT <faistauer.tomas@gmail.com>
2022-08-08 17:45:32 +0200

    how do i fix the image changer to work prettier?

--- image-changer.js
+++ image-changer.js
@@ -1,12 +1,12 @@
-let potatoImage = document.querySelector("#potatoSad");
+let potatoImage = document.querySelector(".potato");
 
-potatoImage.onclick = function() {
-    let imageSrc = potatoImage.getAttribute("src")
-    if (imageSrc === "potato-sad.jpg") {
+potatoImage.addEventListener('click', () => {
+    let potatoIamgeAttribute = potatoImage.getAttribute('src');
+    if (potatoIamgeAttribute === 'potato-sad.jpg') {
         potatoImage.setAttribute('src', 'potato-happy.jpg');
-        potatoImage.setAttribute('style', 'height:100px;width:auto');
+        potatoImage.setAttribute('class', '.potato')
     } else {
         potatoImage.setAttribute('src', 'potato-sad.jpg');
-        potatoImage.setAttribute('style', 'height:150px;width:auto');
+        potatoImage.setAttribute('class', '.potato')
     }
-}
+})
